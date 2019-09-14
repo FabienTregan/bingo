181b03139e80b0d5539c8de1122a5db8776f2d21
Fabien Tregan <treguy@free.fr>
2019-09-14 14:24:26 +0200

    Updates content of the grid.

--- contenu.js
+++ contenu.js
@@ -2,20 +2,20 @@ var title = "Le bingo de KrysaliaH - Une grille complétée en un stream = 1 cof
 var row = 4	// nombre de lignes
 var col = 4	// nombre de colones
 var bingo = [
-"Tu me fais rougir",
-"Compte avec KrysaliaH !",
+"Rougie",
+"\"Compte avec KrysaliaH !\"",
 "",
-"un gros mot",
+"Gros mot",
 "",
-"Syndrome de l'imposteur",
+"Syndrome de l'imposteuse",
 "Rangement Tout Sous La Main",
 "Evoque son passé de prof",
-"Non mais c'est pas vrai !",
-"Fromage",
+"\"Non mais c'est pas vrai !\"",
+"Parle de fromage",
 "",
-"",
+"Une #RefDeVieux (Krys ou le chat)",
 "",
 "Violet",
-"Tu sors !",
+"\"Tu sors !\"",
 "",
 ]	// cases du bingo
